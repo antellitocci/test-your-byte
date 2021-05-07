c712b6eea8961024d1a2302f0b9b11e9fb06d441
raddymaster <antellitocci@gmail.com>
2021-05-06 20:02:35 -0400

    initial content layout and javascript psuedo code

---
+++ assets/javascript/script.js
@@ -0,0 +1,37 @@
+var questionBank =[
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+];
+
+//Set the stage for questions to be served
+$("#ready").click(function(){
+    console.log("clicked ready");
+    $("#intro").hide();
+    serveQuestions();
+    $("#questions").show();
+});
+
+//begin serving questions
+function serveQuestions()
+{   
+    //randomly select a question from question bank and serve it
+    //delete from question bank after answer
+    //keep counter of total answered questions
+    //keep counter of total right answers
+    //keep track of score
+};
+
+//check answers
+function checkAnswers()
+{
+
+};
+
